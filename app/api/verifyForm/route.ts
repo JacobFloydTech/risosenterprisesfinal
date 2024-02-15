4984ccfc01b228475b82345e7c812aa8f0d6a3d4
Jacob Floyd <jacob@Jacobs-MacBook-Air.local>
2024-02-15 23:35:08 +1300

    x

--- app/api/verifyForm/route.ts
+++ app/api/verifyForm/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) { 
     const { token } = await req.json();
-    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY!}&response=${token}`)
+    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=6Ld84ywpAAAAAMI0z8S71FiT3orE1scD8uumYiGk&response=${token}`)
     const data = await response.json();
     console.log(data);
     return NextResponse.json(data);
