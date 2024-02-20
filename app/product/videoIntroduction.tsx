9eba7eea74783eadc4ff2393581117957bccda4a
Jacob Floyd <41782145+JacobFloydTech@users.noreply.github.com>
2024-02-20 20:38:10 +1300

    static

--- app/product/videoIntroduction.tsx
+++ app/product/videoIntroduction.tsx
@@ -32,7 +32,7 @@ export default function VideoIntroduction({setter}: { setter: Function}) {
     }
     function main() {
         if (!grid.current || !buttons.current) { return }
-        
+
         const el = document.getElementById('videoContainer');
 
         let player = document.getElementById('video') as HTMLVideoElement;
@@ -43,7 +43,7 @@ export default function VideoIntroduction({setter}: { setter: Function}) {
             document.getElementById('videoText')?.classList.add('videoText');
             buttons.current.classList.add('buttons');
             const children = Array.from(grid.current?.children) as Array<any>;
-            children.forEach((e) => { animate(Array.from(e?.children)) })
+    
             document.getElementById('video')?.classList.add('fadeOut')
         }, 7500);
         setTimeout(() => {
@@ -58,13 +58,17 @@ export default function VideoIntroduction({setter}: { setter: Function}) {
             document.getElementById('videoText')?.classList.add('videoText');
             buttons.current.classList.add('buttons');
             const children = Array.from(grid.current?.children) as Array<any>;
-            children.forEach((e) => { animate(Array.from(e?.children)) })
+
             document.getElementById('video')?.classList.add('fadeOut')
         }, 2500);
     }
-    function animate(array: Array<HTMLParagraphElement>) {
+    function animate() {
+        const array = Array.from(document.getElementById('chemicals')?.children ?? []);
+
+        if (!array) return
         array.forEach((e, i) => {
             setTimeout(() => {
+                console.log(e);
                 e.classList.add('chemicalSlide');
             }, (i / 2 + 0.5) * 1000);
         })
@@ -81,51 +85,29 @@ export default function VideoIntroduction({setter}: { setter: Function}) {
     const particulates = ['E. coli', 'Campylobacter', 'Cryptosporidium', 'Salmonella', 'Giardia']
     const chemicals = ['Nitrates', 'Phosphates', 'Hormones', 'Microplastics', 'Cyanobacteria']
     return (
-        <div className="py-4 flex flex-col items-center justify-center w-full relative">
-            <button ref={button} onClick={() => {start(); button.current.classList.add('fadeOut'); button.current.classList.remove('squeezeButton')}} className="bg-yellow-300 mt-12 text-white text-2xl px-4 py-2 rounded-full squeezeButton overflow-hidden z-40 transition hover:scale-110 duration-100">Squeeze me</button>
-            <div id='videoContainer' className=" w-full h-[550px] xl:h-[700px] 2xl:h-[950px]  relative flex flex-col justify-center items-center ">
-                <video onError={() => loadImage()} preload="auto" disableRemotePlayback id='video' className="w-full opacity-0 h-full object-cover md:object-contain top-0 left-0 absolute" playsInline controls={false} muted>
-                    <source type="video/webm" src="/wdasdas.webm"/>
-            
-                </video>
-                <img id="waicorderRenderImage" src="waicorderRender.png" className="w-full md:w-[95%] mx-auto opacity-0 absolute top-0 left-0"/>
-                <div id='backgroundFilter' className="z-30 px-4 py-2 rounded-3xl absolute top-0 left-1/2 -translate-x-1/2 w-full md:w-1/2 bg-[rgba(255,240,0,0.2)] backdrop-blur-lg opacity-0 h-full" />
-                <div className="z-40 text-white md:text-lg font-bold w-3/4 md:w-2/3 2xl:w-1/2  mx-auto text-center" >
-                    <div id='videoText' className="opacity-0">
-                        <p className=" xl:text-4xl 2xl:text-5xl 3xl:text-6xl md:text-xl  text-lg font-bold ">The Waicorder</p>
-                        <p className="2xl:text-2xl xl:text-xl md:text-lg tex-sm py-2 font-semibold">Wāi Ora. Tāngata Ora</p>
-                        <div className="w-full mx-auto my-2 h-[3px] bg-white 3xl:my-4 " />
-                        <p className="text-sm xl:text-lg 2xl:text-3xl 3xl:text-5xl py-2 xl:py-4 2xl:py-6 3xl:py-12">Water Testing in Seconds</p>
-                    </div>
-                    <div ref={grid} className="grid grid-cols-2 md:-4  xl:w-3/4  mx-auto">
-                        <ul className="flex flex-col items-center md:space-y-2 2xl:space-y-4">
-                            {particulates.map((e) => {
-                                return <p className="lg:text-left text-sm md:text-xl xl:text-2xl 2xl:text-4xl 3xl:text-5xl md:w-1/2 mx-auto text-center opacity-0">{e}</p>
-                            })}
-                        </ul>
-                        <ul className="flex flex-col items-center  md:space-y-2 2xl:space-y-4">
-                            {chemicals.map((e) => {
-                                return <p className="lg:text-left text-sm md:text-xl xl:text-2xl 2xl:text-4xl 3xl:text-5xl md:w-1/2 mx-auto text-center opacity-0">{e}</p>
-                            })}
-                        </ul>
-                    </div>
-        
-                    <div ref={buttons} className=" xl:w-3/4 2xl:w-2/3 pt-4 text-white z-40 flex justify-center items-center space-y-8 md:space-y-0  md:text-lg 2xl:text-xl 3xl:text-2xl my-2  md:my-4 xl:my-12   opacity-0 mx-auto">
-                        <div className="flex flex-col items-center space-y-2 justify-center">
-                            <p className="text-xs py-1 md:py-4 xl:py-6 md:text-base xl:text-xl">Detecting waterborne pathogens early is critical.<br/>
-It means more than freedom and independence; <br/>
+        <div className="py-4 flex text-white flex-col items-center justify-center w-full relative">
+           <div className="bg-yellow-500 bg-opacity-30 backdrop-blur-xl text-white xl:text-4xl md:text-2xl text-center items-center justify-center w-1/2 py-4 rounded-2xl flex flex-col md:space-y-4">
+                <p>The Waicorder</p>
+                <p>Wāi Ora. Tāngata Ora</p>
+           </div>
+           <img src="/waicorderRender.png" className="md:w-3/4 xl:w-2/3"/>
+           <div className="bg-yellow-500 bg-opacity-30 backdrop-blur-xl p-4 rounded-2xl w-1/3 items-center justify-center flex flex-col">
+           <p className="text-3xl py-2">Waiter Testing in Seconds</p>
+           <ul className="text-2xl list-disc" id='chemicals'>
+                {particulates.map((e) => <li className="">{e}</li>)}
+                {chemicals.map(e => <li className="">{e}</li>)}
+           </ul>
+           </div>
+           <p className="text-4xl leading-[50px] bg-yellow-500 bg-opacity-30 backdrop-blur-xl p-4 rounded-2xl my-12">Detecting waterborne pathogens early is critical. <br/>
+It means more than freedom and independence;<br/>
 it will be the next step for all people on earth. <br/>
 This generation and the next generation. <br/>
 This planet and others to come. <br/>
 It will be the fundamental step for unity and peace. <br/>
 That's why we develop the Waicorder.</p>
-                            <p className="text-sm md:text-xl 2xl:text-lg ">Get in touch if you want to help, join or know more:</p>
-                            <button onClick={() => window.location.href = '/contact'} className="bg-[#f2cc00] rounded-full border-white border-2 px-4 py-2 transition duration-200 hover:scale-105 hover:[background-size:200%] xl:hover:scale-110  p-2  3xl:px-8 3xl:py-4">Get in contact </button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+        <p>
+Get in touch if you want to help, join or know more:</p>
+<button className="rounded-full border-2 border-white text-white bg-yellow-400 py-2 px-4 text-2xl hover:scale-105 transition-all duration-100 ">Get in contact</button>
         </div>
     )
 }
