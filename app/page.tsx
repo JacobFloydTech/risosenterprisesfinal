d5712b2efe2525f38952f72f6307cd5dba7379b3
Jacob Floyd <41782145+JacobFloydTech@users.noreply.github.com>
2024-02-20 20:43:14 +1300

    x

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,11 @@ import { useState } from "react";
 import Loading from "./loading";
 
 export default function Page() {
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(true)
 
   return (
     <div id='main' className="flex flex-col items-center jutify-center pt-24  h-auto w-full">
      
-
-      <ThreeJSBackground setLoaded={setLoaded}/>
       {loaded ? 
       <div>
         <Header/>
